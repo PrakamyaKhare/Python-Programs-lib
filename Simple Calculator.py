42b3d9ccab507effa52dc0fe6a78bc8bb4f7cdf4
PrakamyaKhare <53349174+PrakamyaKhare@users.noreply.github.com>
2021-01-08 10:37:54 +0530

    Create Simple Calculator.py

---
+++ Simple Calculator.py
@@ -0,0 +1,32 @@
+#simple clculator
+
+str = 'ans = '
+
+def add(a,b):
+	c  = a + b
+	print(str, c)
+	
+def subtract(a,b):
+	c = a - b
+	print(str,c)
+	
+def div(a,b):
+	c = a/b
+	print(str,c)	
+
+def mul(a,b):
+	c = a * b 
+	print(str,c)
+
+op = input("enter operator either + -  * or / ")
+a = int(input("enter first operand "))
+b = int(input("enter another operand "))
+
+if  op == '+':
+	add(a,b)
+elif op == '-':
+	subtracr(a,b)
+elif op == '*':
+	mul(a,b)
+elif op == '/':
+	div(a,b)
